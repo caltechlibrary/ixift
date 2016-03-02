1ab24e5a8dec7c7f8502db869f4ed3a29ccd1ae1
R. S. Doiel <rsdoiel@gmail.com>
2016-03-02 11:45:01 -0800

    Initial Setup

---
+++ schema_test.go
@@ -0,0 +1,52 @@
+//
+// schema_test.go - test data structures for ixift package.
+//
+// @author R. S. Doiel, <rsdoiel@caltech.edu>
+//
+//
+// Copyright (c) 2016, Caltech
+// All rights not granted herein are expressly reserved by Caltech.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+package ixift
+
+import (
+	"log"
+	"testing"
+)
+
+//
+// Schema describes the memory structure used to reading and generating IIIF and IxIF manifest.
+//
+func TestStructsDefinitions(t *testing.T) {
+	log.Println("Testing structure definitions")
+
+	m := new(Manifest)
+	if m == nil {
+		t.Errorf("new(Manifest) failed")
+	}
+
+	s := new(Sequence)
+	if s == nil {
+		t.Errorf("new(Sequence) failed")
+	}
+
+	c := new(Canvas)
+	if c == nil {
+		t.Errorf("new(Canvas) failed")
+	}
+
+	content := new(Content)
+	if content == nil {
+		t.Errorf("new(Content) failed")
+	}
+}
